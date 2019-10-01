546474c2ebb1563fde9ce21224e5046d9403d886
Patrick Bosman <bsmn.ptrck@gmail.com>
2019-10-01 20:04:36 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -2,26 +2,26 @@ require "yaml"
 
 def load_library(file)
   emoticons = YAML.load_file(file)
-  new_hash = {"get_emoticon": {}, "get_meaning": {}}
+  new_hash = {"get_meaning": {}, "get_emoticon": {}}
   
   
   
   new_hash
 end
 
-def get_japanese_emoticon(file, emo)
+def get_japanese_emoticon(file, emoji)
   call = load_library(file)
-  if call[:get_emoticon][emo]
-    call[:get_emoticon][emo]
+  if call[:get_emoticon][emoji]
+    call[:get_emoticon][emoji]
   else 
     return "Sorry, that emoticon was not found"
   end
 end
 
-def get_english_meaning(file, emo)
+def get_english_meaning(file, emoji)
   call = load_library(file)
-  if call[:get_meaning][emo]
-    call[:get_meaning][emo]
+  if call[:get_meaning][emoji]
+    call[:get_meaning][emoji]
   else
     return "Sorry, that emoticon was not found"
   end
