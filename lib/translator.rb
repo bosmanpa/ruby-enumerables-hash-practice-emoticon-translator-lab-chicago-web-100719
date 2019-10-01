6dff76f8b571823b5612b088a5455bcd25d885f6
Patrick Bosman <bsmn.ptrck@gmail.com>
2019-10-01 19:51:34 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -9,10 +9,10 @@ def load_library(file)
   new_hash
 end
 
-def get_japanese_emoticon
+def get_japanese_emoticon(file, jemo)
   # code goes here
 end
 
-def get_english_meaning
+def get_english_meaning(file, eemo)
   # code goes here
 end
